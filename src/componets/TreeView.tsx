145c121a8c6ac2348303817336daf983dffd9a52
Jakub Jankiewicz <jcubic@onet.pl>
2022-04-12 19:39:13 +0200

    refactor TreeView

--- src/componets/TreeView.tsx
+++ src/componets/TreeView.tsx
@@ -10,46 +10,53 @@ type TreeNodeCallback<T> = (item: T) => void;
 
 type TreeNodeProps<T> = {
   node: T;
-  onChange: TreeNodeCallback<T>;
+  onChange?: TreeNodeCallback<T>;
 };
 
 const TreeNode = <T extends TreeNodeT<T>,>({ node, onChange }: TreeNodeProps<T>) => {
   const { name, childNodes } = node;
   if (!childNodes) {
-    return <span onClick={() => onChange(node)}>{name}</span>;
+    return <span onClick={() => onChange?.(node)}>{name}</span>;
   }
   return (
     <details open>
       <summary>{name}</summary>
-      <ul>
-        {childNodes.map((node) => {
-          return (
-            <li key={node.id}>
-              <TreeNode node={node} onChange={onChange} />
-            </li>
-          );
-        })}
-      </ul>
+      <TreeList nodes={childNodes} onChange={onChange}/>
     </details>
   );
 };
+
+
+type TreeList<T> = Array<TreeNodeT<T>>;
+
+type TreeListProps<T> = {
+  nodes: Array<T>;
+  onChange?: TreeNodeCallback<T>;
+};
+
+const TreeList = <T extends TreeNodeT<T>,>({ nodes, onChange }: TreeListProps<T>) => {
+  return (
+    <ul>
+      {nodes.map((node) => {
+        return (
+          <li key={node.id}>
+            <TreeNode node={node} onChange={onChange} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
 type TreeViewProps<T> = {
   data: Array<T>;
-  className?: string;
-  onChange: TreeNodeCallback<T>;
+  onChange?: TreeNodeCallback<T>;
 };
+
 const TreeView = <T extends TreeNodeT<T>,>({ data, onChange }: TreeViewProps<T>) => {
   return (
     <div className={tree}>
-      <ul>
-        {data.map((node) => {
-          return (
-            <li key={node.id}>
-              <TreeNode node={node} onChange={onChange} />
-            </li>
-          );
-        })}
-      </ul>
+      <TreeList nodes={data} onChange={onChange}/>
     </div>
   );
 };
